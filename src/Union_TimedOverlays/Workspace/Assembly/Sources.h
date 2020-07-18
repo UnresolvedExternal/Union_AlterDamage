581e9bd7a3733c3c44768de9971d844ab5f44f36
TopLayer <vmi1401bsv04@gmail.com>
2020-07-18 21:12:21 +0500

    Union_TimedOverlays released

---
+++ src/Union_TimedOverlays/Workspace/Assembly/Sources.h
@@ -0,0 +1,10 @@
+#include "Workspace\Settings\CPluginSettings.cpp"
+
+#include "Workspace\Savegame\CSGGlobal.cpp"
+#include "Workspace\Savegame\CSGInstanceSet.cpp"
+#include "Workspace\Savegame\CSGInventoryMonitor.cpp"
+#include "Workspace\Savegame\CSGOverlaySet.cpp"
+
+#include "Workspace\Utility\Engine\Misc.cpp"
+
+#include "Workspace\Entry\SaveOverlays.cpp"
